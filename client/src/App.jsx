1ad8ffc1e35dd54b3c2b91abd846f9e2537b007a
Akash Rajan <akashrajan93@gmail.com>
2023-07-28 15:45:38 -0400

    initialize repo

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './index.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1 className="text-red-500">lol</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
