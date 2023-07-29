fd6bdd6c9b2ce60f2fd09d1f18f6f4194658bb78
Akash Rajan <akashrajan93@gmail.com>
2023-07-29 16:07:34 -0400

    added useEffect and imported axios data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,36 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './index.css';
+import axios from 'axios';
+import { Outlet, Link } from 'react-router-dom';
+import animalsLoader from './loaders/animalsLoader.jsx';
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0);
+  const [animals, setAnimals] = useState([]);
+
+  const fetch = () => {
+    axios.get('http://localhost:3000/').then((response) => {
+      console.log('AXIOS RESPONSE IN APP', response.data);
+      setAnimals(response.data.animals);
+    });
+  };
+
+  // let allAnimals = useLoaderData();
+
+  // useEffect(() => {
+  //   console.log(animalsLoader());
+  // }, []);
+
+  useEffect(() => {
+    fetch();
+  }, []);
 
   return (
-    <>
-      <div>
-        <h1 className="text-red-500">lol</h1>
-      </div>
-    </>
+    <div>
+      <Link to="/home">Home Button</Link>
+      <Outlet />
+    </div>
   );
-}
+};
 
 export default App;
