692a3ef6c6cae87266543232f846e422a3435754
Akash Rajan <akashrajan93@gmail.com>
2023-07-31 15:52:54 -0400

    added geolocation services to hit petfinder API with location

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,31 +8,42 @@ import Stack from '@mui/material/Stack';
 const App = () => {
   const [count, setCount] = useState(0);
   const [animals, setAnimals] = useState([]);
+  const [longitude, setlongitude] = useState(null);
+  const [latitude, setlatitude] = useState(null);
 
   const fetch = () => {
-    axios.get('http://localhost:3000/').then((response) => {
-      console.log('AXIOS RESPONSE IN APP', response.data);
-      setAnimals(response.data.animals);
+    navigator.geolocation.getCurrentPosition((location) => {
+      axios
+        .get(
+          `https://api.opencagedata.com/geocode/v1/json?q=${location.coords.latitude}+${location.coords.longitude}&key=dfb6f1c5097f40d883ef5fee3c7e97a3`
+        )
+        .then((results) => {
+          const postcode = results.data.results[0].components.postcode;
+          return axios.get('http://localhost:3000/animals', {
+            params: {
+              zip: postcode,
+            },
+          });
+        })
+        .then((response) => {
+          console.log('AXIOS RESPONSE IN APP', response.data);
+          setAnimals(response.data.animals);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     });
   };
-
-  // let allAnimals = useLoaderData();
-
-  // useEffect(() => {
-  //   console.log(animalsLoader());
-  // }, []);
 
   useEffect(() => {
     fetch();
   }, []);
 
   return (
-    // <div className="m-5">
     <Stack m={2}>
       <Link to="/home">Home Button</Link>
       <Outlet />
     </Stack>
-    // </div>
   );
 };
 
