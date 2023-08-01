f4cfd68942f083301fa04298004c049cdfd33606
Akash Rajan <akashrajan93@gmail.com>
2023-08-01 19:47:56 -0400

    added confetti and fixed git conflict

--- client/src/components/MemoryGame/Game.jsx
+++ client/src/components/MemoryGame/Game.jsx
@@ -1,11 +1,10 @@
-import GameCard from "./GameCard.jsx";
-import { useState, useEffect } from "react";
-import { useRouteLoaderData } from "react-router-dom";
+import GameCard from './GameCard.jsx';
+import { useState, useEffect } from 'react';
+import { useRouteLoaderData } from 'react-router-dom';
 
 import GameFinished from './GameFinished.jsx';
 
 import io from 'socket.io-client';
-
 
 const Game = () => {
   //TODO add styling:
@@ -16,12 +15,11 @@ const Game = () => {
   // ability to turn sound on/off
   // add cat img to front of card: https://media.istockphoto.com/id/1303646726/vector/doodle-cat-mustache-icon-isolated-on-white-outline-hand-drawing-art-line-sketch-logo-animal.jpg?s=612x612&w=0&k=20&c=41W_rC17pyfN_xNxxNIDL1BBTWXFLqtIYf9LL6z1qPk=
 
-
-  const [firstCard, setFirstCard] = useState("");
-  const [secondCard, setSecondCard] = useState("");
+  const [firstCard, setFirstCard] = useState('');
+  const [secondCard, setSecondCard] = useState('');
   const [matches, setMatches] = useState([]);
-  const petsData = useRouteLoaderData("root");
-  let petsArr = petsData.data.animals.sort(() => Math.random() - 0.5)
+  const petsData = useRouteLoaderData('root');
+  let petsArr = petsData.data.animals.sort(() => Math.random() - 0.5);
   let firstSelectedPets = petsArr.slice(0, 6);
   const [turn, setTurn] = useState(0);
   const [waiting, setWaiting] = useState(false);
@@ -57,12 +55,11 @@ const Game = () => {
   ]);
 
   useEffect(() => {
-    setPetCards(unshuffledCards => {
-      return unshuffledCards.sort(() => Math.random() - 0.5)
-    })
+    setPetCards((unshuffledCards) => {
+      return unshuffledCards.sort(() => Math.random() - 0.5);
+    });
     setLoading(false);
-  }, [])
-
+  }, []);
 
   const setFlipped = (petId, index) => {
     setPetCards((pets) => {
@@ -92,8 +89,8 @@ const Game = () => {
   useEffect(() => {
     if (firstCard && secondCard) {
       if (firstCard === secondCard) {
-        setMatchCount((matchCount) => matchCount += 2)
-        console.log('matchcount: ', matchCount)
+        setMatchCount((matchCount) => (matchCount += 2));
+        console.log('matchcount: ', matchCount);
         setPetCards((currPetCards) => {
           return currPetCards.map((currPetCard) => {
             if (currPetCard.id === firstCard) {
@@ -102,11 +99,11 @@ const Game = () => {
             return currPetCard;
           });
         });
-        setFirstCard("");
-        setSecondCard("");
+        setFirstCard('');
+        setSecondCard('');
       } else {
-        setFirstCard("");
-        setSecondCard("");
+        setFirstCard('');
+        setSecondCard('');
         wait();
       }
     }
@@ -128,17 +125,11 @@ const Game = () => {
     }, 2500);
   };
 
-
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
-
   useEffect(() => {}, [firstCard, secondCard, turn]);
   useEffect(() => {
     // setPetCards([...petCards, petCardArray]);
     //setPetCards([...petCardArray, ...petCardArray])
-  }, [])
+  }, []);
 
   useEffect(() => {
     const socket = io.connect('http://localhost:3000', {
@@ -148,13 +139,13 @@ const Game = () => {
       transports: ['websocket'],
       agent: false,
       upgrade: false,
-      rejectUnauthorized: false
+      rejectUnauthorized: false,
     });
     // const socket = io();
 
     console.log('connecting to server soon...');
 
-    socket.emit("msg", 5, "4", { 7: Uint8Array.from([8]) });
+    socket.emit('msg', 5, '4', { 7: Uint8Array.from([8]) });
 
     socket.on('hello', () => {
       console.log('Connected to the server');
@@ -165,55 +156,58 @@ const Game = () => {
 
       // // Handle the "player_ready" event received from the server
       // socket.on('player_ready', (data) => {
-        //   console.log('Player', data.player, 'is ready');
-        // });
+      //   console.log('Player', data.player, 'is ready');
+      // });
 
-        // // Example: Sending a "make_move" event to the server with move data
-        // const moveData = {move:'move-data-here'};
-        // socket.emit('make_move', moveData);
+      // // Example: Sending a "make_move" event to the server with move data
+      // const moveData = {move:'move-data-here'};
+      // socket.emit('make_move', moveData);
 
-        // // Handle the "move_made" event received from the server
-        // socket.on('move_made', (data) => {
-          //   console.log('Player', data.player, 'made a move:', data.move);
-          // });
-        });
+      // // Handle the "move_made" event received from the server
+      // socket.on('move_made', (data) => {
+      //   console.log('Player', data.player, 'made a move:', data.move);
+      // });
+    });
 
-        // socket.on('disconnect', () => {
-          //   console.log('Disconnected from the server');
-          // });
+    // socket.on('disconnect', () => {
+    //   console.log('Disconnected from the server');
+    // });
 
-          // Clean up the socket connection when the component is unmounted
-          return () => {
-            socket.disconnect();
-          };
-        }, []);
+    // Clean up the socket connection when the component is unmounted
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
 
   if (loading) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
   return (
     <div>
-
-      {!gameFinshed ?
-      <div>
-        <h1 className="text-3xl font-bold underline m-1/2">Flip and match!</h1>
-        <div className="flex flex-row gap-15 flex-wrap">
-          {petCards.map((petCard, i) => (
-            <GameCard
-            key={i}
-            pet={petCard}
-            setFlipped={setFlipped}
-            setTurn={setTurn}
-            turn={turn}
-            waiting={waiting}
-            setFirstCard={setFirstCard}
-            setSecondCard={setSecondCard}
-              matches={matches}
-            />
-          ))}
+      {!gameFinshed ? (
+        <div>
+          <h1 className="text-3xl font-bold underline m-1/2">
+            Flip and match!
+          </h1>
+          <div className="flex flex-row gap-15 flex-wrap">
+            {petCards.map((petCard, i) => (
+              <GameCard
+                key={i}
+                pet={petCard}
+                setFlipped={setFlipped}
+                setTurn={setTurn}
+                turn={turn}
+                waiting={waiting}
+                setFirstCard={setFirstCard}
+                setSecondCard={setSecondCard}
+                matches={matches}
+              />
+            ))}
+          </div>
         </div>
-      </div>
-      : <GameFinished />}
+      ) : (
+        <GameFinished />
+      )}
     </div>
   );
 };
