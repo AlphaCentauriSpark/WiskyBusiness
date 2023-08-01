51032a139734a368a56749f5aa394606a3a711e5
Akash Rajan <akashrajan93@gmail.com>
2023-08-01 16:20:32 -0400

    added filtering for catalog page by gender, species, size

--- client/src/components/Catalog.jsx
+++ client/src/components/Catalog.jsx
@@ -1,28 +1,92 @@
 import axios from 'axios';
-import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 import Card from './Card.jsx';
 import { AnimalContext } from '../App';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 const Catalog = () => {
+  const [speciesFilter, setSpeciesFilter] = useState('all');
+  const [sizeFilter, setSizeFilter] = useState('all');
+  const [genderFilter, setGenderFilter] = useState('all');
   const animals = useContext(AnimalContext);
 
   console.log(animals);
   return (
-    <Stack direction="row" spacing={2}>
-      {animals.map((animal) => {
-        if (animal.primary_photo_cropped) {
-          return (
-            <Card
-              photo={animal.primary_photo_cropped.small}
-              name={animal.name}
-              species={animal.species}
-              gender={animal.gender}
-            />
-          );
-        }
-      })}
-    </Stack>
+    <>
+      <Box>
+        <FormControl variant="outlined" sx={{ minWidth: 120 }}>
+          <InputLabel id="Species Filter">Species</InputLabel>
+          <Select
+            labelId="Species Filter"
+            id="Species Filter"
+            value={speciesFilter}
+            onChange={(event) => setSpeciesFilter(event.target.value)}
+            label="Species"
+          >
+            <MenuItem value="all">All</MenuItem>
+            <MenuItem value="Cat">Cat</MenuItem>
+            <MenuItem value="Dog">Dog</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl variant="outlined" sx={{ minWidth: 120 }}>
+          <InputLabel id="Gender Filter">Gender</InputLabel>
+          <Select
+            labelId="Gender Filter"
+            id="Gender Filter"
+            value={genderFilter}
+            onChange={(event) => setGenderFilter(event.target.value)}
+            label="Gender"
+          >
+            <MenuItem value="all">All</MenuItem>
+            <MenuItem value="Male">Male</MenuItem>
+            <MenuItem value="Female">Female</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl variant="outlined" sx={{ minWidth: 120 }}>
+          <InputLabel id="Species Filter">Size</InputLabel>
+          <Select
+            labelId="Size Filter"
+            id="Size Filter"
+            value={sizeFilter}
+            onChange={(event) => setSizeFilter(event.target.value)}
+            label="Size"
+          >
+            <MenuItem value="all">All</MenuItem>
+            <MenuItem value="Small">Small</MenuItem>
+            <MenuItem value="Medium">Medium</MenuItem>
+            <MenuItem value="Large">Large</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+      <Grid container spacing={2}>
+        {animals
+          .filter(
+            (animal) =>
+              (genderFilter === 'all' || animal.gender === genderFilter) &&
+              (speciesFilter === 'all' || animal.species === speciesFilter) &&
+              (sizeFilter === 'all' || animal.size === sizeFilter)
+          )
+          .map((animal) => {
+            if (animal.primary_photo_cropped) {
+              return (
+                <Grid item xs={6} sm={3} md={2} lg={1.5}>
+                  <Card
+                    photo={animal.primary_photo_cropped.small}
+                    name={animal.name}
+                    species={animal.species}
+                    gender={animal.gender}
+                  />
+                </Grid>
+              );
+            }
+          })}
+      </Grid>
+    </>
   );
 };
 
--- server/controllers/routes.js
+++ server/controllers/routes.js
@@ -41,7 +41,7 @@ module.exports.getAnimals = (req, res) => {
     .then((tokenObj) => {
       console.log(tokenObj);
       let token = tokenObj.data.token_type + ' ' + tokenObj.data.access_token;
-      let category = `animals/?location=${req.query.zip}&limit=25`;
+      let category = `animals/?location=${req.query.zip}&limit=75`;
       axios({
         method: 'get',
         url: category,
