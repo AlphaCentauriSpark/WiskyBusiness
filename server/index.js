0d9e40f73aca5e8450a1cb305eca6acfba8b307f
Zach Morrison <zacharymorrison92@gmail.com>
2023-08-02 15:10:05 -0400

    made more progress on socket.io server

--- server/index.js
+++ server/index.js
@@ -36,52 +36,54 @@ const io = new Server(server);
 const rooms = new Map();
 
 io.on('connection', (socket) => {
-  // console.log('New player connected:', socket.id);
+  console.log('New player connected:', socket.id);
   console.log('connected');
   
-  socket.emit('helooooo', 1, '2', { 3: Buffer.from([4])});
+  // socket.emit('helooooo', 1, '2', { 3: Buffer.from([4])});
 
-  socket.on('msg', (data) => {
-    console.log('message!!')
-    io.sockets.emit('hello', data);
-  })
+  // socket.on('msg', (data) => {
+  //   console.log('message!!')
+  //   io.sockets.emit('hello', data);
+  // })
 
 
-  // Matchmaking and room creation logic
-  // let room;
-  // if (!room) {
-  //   room = createNewRoom();
-  //   socket.join(room);
-  // } else {
-  //   socket.join(room);
-  // }
+  //Matchmaking and room creation logic
+  let room;
+  if (!room) {
+    room = createNewRoom();
+    socket.join(room);
+  } else {
+    socket.join(room);
+  }
+ 
+  // Notify the clients that they are connected
+  io.to(room).emit('players_connected', { room });
 
-  // // Notify the clients that they are connected
-  // //io.to(room).emit('players_connected', { room });
+  // Handle client events
+  socket.on('ready', () => {
+    // Handle the "ready" event, indicating the player is ready to start the game
+    console.log('PLAYER READY');
+    io.to(room).emit('player_ready', { player: socket.id });
+  });  
 
-  // // Handle client events
-  // socket.on('ready', () => {
-  //   // Handle the "ready" event, indicating the player is ready to start the game
-  //   io.to(room).emit('player_ready', { player: socket.id });
-  // });
+  socket.on('make_move', (move) => {
+    // Handle the "make_move" event with the move data from the client
+    // Update game state and notify all clients about the move
+    console.log('MOVE HAS BEEN MADE');
+    io.to(room).emit('move_made', { player: socket.id, move });
+  });
 
-  // socket.on('make_move', (move) => {
-  //   // Handle the "make_move" event with the move data from the client
-  //   // Update game state and notify all clients about the move
-  //   io.to(room).emit('move_made', { player: socket.id, move });
-  // });
-
-  // socket.on('disconnect', () => {
-  //   console.log('Player disconnected:', socket.id);
-  //   // Implement logic to handle a player disconnecting mid-game (if needed)
-  // });
+  socket.on('disconnect', () => {
+    console.log('Player disconnected:', socket.id);
+    // Implement logic to handle a player disconnecting mid-game (if needed)
+  });
 });
 
-// function createNewRoom() {
-//   const room = 'room_' + Math.random().toString(36).substr(2, 4);
-//   rooms.set(room, { players: 0 });
-//   return room;
-// }
+function createNewRoom() {
+  const room = 'room_' + Math.random().toString(36).substr(2, 4);
+  rooms.set(room, { players: 0 });
+  return room;
+}
 
 
 
