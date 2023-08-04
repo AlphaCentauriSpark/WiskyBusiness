db4bb7f389cf307456916986a57face127893e29
Akash Rajan <akashrajan93@gmail.com>
2023-08-04 10:20:57 -0400

    changed the deployed server address

--- client/src/loaders/petsLoader.jsx
+++ client/src/loaders/petsLoader.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const petsLoader = () => {
   return axios
-    .get('ip-172-31-30-86.us-west-1.compute.internal:3000')
+    .get('54.176.189.181:3000')
     .then((response) => {
       return response.data;
     })
