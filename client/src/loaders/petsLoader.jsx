eb7cb9c1901c1aea90158ba3d33f6f9e3b89122f
Akash Rajan <akashrajan93@gmail.com>
2023-08-03 20:02:13 -0400

    added one more redirect to deployed server

--- client/src/loaders/petsLoader.jsx
+++ client/src/loaders/petsLoader.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const petsLoader = () => {
   return axios
-    .get('http://localhost:3000/')
+    .get('wiskey-business-server.vercel.app:3000/')
     .then((response) => {
       return response.data;
     })
