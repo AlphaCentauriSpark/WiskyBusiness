059f1c9eff38fcfdab05ee011488cb7136f593d5
Lauren Swersky <lauren.swersky@gmail.com>
2023-08-10 16:26:54 -0400

    vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: 'ec2-3-142-194-170.us-east-2.compute.amazonaws.com',
+  },
   plugins: [react()],
 })
