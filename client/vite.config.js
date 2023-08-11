4e14332467ad3208f3e35e5172f722f52cf97af3
Akash Rajan <akashrajan93@gmail.com>
2023-08-10 20:41:20 -0400

    added public path

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: '0.0.0.0',
-    port: 5173
+    port: 5173,
+    public: 'http://ec2-54-219-164-184.us-west-1.compute.amazonaws.com/',
   },
   plugins: [react()],
-})
+});
