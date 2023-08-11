cc101cf2d568e3b3679ad6bc57fe8289358e1374
Akash Rajan <akashrajan93@gmail.com>
2023-08-10 20:57:32 -0400

    testing deployment success

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    public: 'http://ec2-54-219-164-184.us-west-1.compute.amazonaws.com/',
+    // public: 'http://ec2-54-219-164-184.us-west-1.compute.amazonaws.com/',
   },
   plugins: [react()],
 });
